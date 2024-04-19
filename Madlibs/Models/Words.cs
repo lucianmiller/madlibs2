500aa19a1506d00176e14221d1a3a8f9aef0604f
Lucian Miller <lucian.miller185@gmail.com>
2024-04-19 11:25:34 -0700

    Add setup

---
+++ Madlibs/Models/Words.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace Madlibs.Models
+{
+  public class Words
+  {
+    public string Person1 { get; set; }
+    public string Person2 { get; set; }
+    public string Animal { get; set; }
+    public string Exclamation { get; set; }
+    public string Verb { get; set; }
+    public string Noun { get; set; }
+  }
+}
